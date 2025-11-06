bdcefda240352d38aee71d8bfa9c35cdb5654243
Angad231 <parabangad123@gmail.com>
2025-11-06 12:29:02 +0530

    added google  map api for better

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -2,7 +2,8 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,9 +20,3 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-  
-  // Initialize Firestore
-  const db = getFirestore(app);
-  
-  // Export db for use in other scripts
-  export { db };
